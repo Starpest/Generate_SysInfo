ed8de02f5556d813be24a64d83d91c90f99c9809
Raghib Mahatab <98721756+Starpest@users.noreply.github.com>
2023-07-26 19:06:25 -0400

    Update net_info.bat

--- net_info.bat
+++ net_info.bat
@@ -1,48 +1,92 @@
 @echo off
-setlocal enabledelayedexpansion
+title NETWORK RESET SCRIPT
+:prompt
+color 1f
+cls
+echo NETWORK RESET SCRIPT
+echo.
+echo Type r to Reset Network
+echo Type p to do a ping test
+echo Type c to go to Command Prompt
+echo Type q to exit
+set /p answer=
+if ‘%answer%’ == ‘r’ goto test
+if ‘%answer%’ == ‘R’ goto test
+if ‘%answer%’ == ‘C’ goto cmd
+if ‘%answer%’ == ‘c’ goto cmd
+if ‘%answer%’ == ‘q’ goto quit
+if ‘%answer%’ == ‘Q’ goto quit
+if ‘%answer%’ == ‘P’ goto ping
+if ‘%answer%’ == ‘p’ goto ping
+pause >nul
 
-python.exe - <<EOF
-import subprocess
-import re
+:test
+cls
+echo _____________________
+echo - RELEASING IP.... -
+echo _____________________
+ipconfig /release >nul
+echo.
+echo Done!
+echo.
+echo _____________________
+echo - RESETTING IP LOG... -
+echo _____________________
+@netsh int ip reset C:WindowsTEMPIPRESETLOG.txt >nul
+echo.
+echo Done!
+echo.
+echo _____________________
+echo - FLUSHING ARP TABLES... -
+echo _____________________
+@arp -d >nul
+echo.
+echo Done!
+echo.
+echo _____________________
+echo - FLUSHING DNS... -
+echo _____________________
+@ipconfig /flushdns >nul
+echo.
+echo Done!
+echo.
+echo _____________________
+echo - RENEWING IP... -
+echo _____________________
+@ipconfig /renew >nul
+echo.
+echo Done!
+echo.
+cls
+echo _____________________
+echo Heres Your Status: -
+echo _____________________
+ipconfig /all
+echo.
+echo Press Any Key to Go To Menu
+pause >nul
+goto prompt
 
-def get_network_info():
-    network_info = {}
+:ping
+cls
+echo _____________________
+echo Starting Ping Test... -
+echo _____________________
+echo.
+ping google.com
+echo.
+echo Press Any Key to goto MENU
+pause >nul
+goto prompt
 
-    try:
-        output = subprocess.check_output(["ipconfig", "/all"]).decode("utf-8")
-        
-        ipv4_pattern = re.compile(r"(IPv4 Address .+?: )([\d.]+)")
-        ipv4_matches = ipv4_pattern.findall(output)
-        network_info["IPv4 Address"] = ", ".join(match[1] for match in ipv4_matches)
-
-        ipv6_pattern = re.compile(r"(IPv6 Address .+?: )([\da-fA-F:]+)")
-        ipv6_matches = ipv6_pattern.findall(output)
-        network_info["IPv6 Address"] = ", ".join(match[1] for match in ipv6_matches)
-
-        dhcp_pattern = re.compile(r"DHCP Server .+?: ([\d.]+)")
-        dhcp_matches = dhcp_pattern.findall(output)
-        network_info["DHCP Servers"] = ", ".join(match for match in dhcp_matches)
-
-        mac_pattern = re.compile(r"Physical Address .+?: ([\da-fA-F-]+)")
-        mac_matches = mac_pattern.findall(output)
-        network_info["MAC Address"] = ", ".join(match for match in mac_matches)
-
-    except subprocess.CalledProcessError:
-        print("Error: Unable to retrieve network information.")
-
-    return network_info
-
-def generate_report(network_info):
-    report = "Network Information Report:\n\n"
-    for key, value in network_info.items():
-        report += f"{key}: {value}\n"
-    return report
-
-if __name__ == "__main__":
-    network_info = get_network_info()
-    report = generate_report(network_info)
-    print(report)
-EOF
-
+:quit
+cls
+echo Exiting...
 pause
+exit
 
+:cmd
+@color 7
+cls
+cmd
+@echo on
